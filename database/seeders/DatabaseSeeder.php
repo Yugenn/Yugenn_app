35db27f4bd21ab8c53bea7c7e34196b45ce5fefb
Yugenn <yuruseajyan@gmail.com>
2021-10-04 11:58:09 +0900

    マイグレーションファイルのダウンメソッドを変更する前のコミット

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ArticleSeeder::class);
+        $this->call(BirthplaceSeeder::class);
+        \App\Models\Article::factory(20)->create();
     }
 }
